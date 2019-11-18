c495ae3ff4812d669d23d9be5ff47746f8a81b80
becting <r.tingley@dal.ca>
2019-11-17 23:06:29 -0400

    clk div

--- craps.vhd
+++ craps.vhd
@@ -43,16 +43,32 @@ component datapath is
 		);
 end component;
 
+component clk_div is
+	PORT
+	(
+		clock_50Mhz				: IN	STD_LOGIC;
+		clock_1MHz				: OUT	STD_LOGIC;
+		clock_100KHz			: OUT	STD_LOGIC;
+		clock_10KHz				: OUT	STD_LOGIC;
+		clock_1KHz				: OUT	STD_LOGIC;
+		clock_100Hz				: OUT	STD_LOGIC;
+		clock_10Hz				: OUT	STD_LOGIC;
+		clock_1Hz				: OUT	STD_LOGIC);
+end component;
+
 signal id7, id711, id2312 : std_logic;
 signal ieq, isp, iroll : std_logic;
+signal iclk: std_logic;
 
 begin
 
+	myclock : component clk_div port map (clock_50Mhz => clk, clock_1kHz => iclk);
+
 	-- instantiate the controller --
-	control : component controller port map(enter => enter, rst => rst, clk => clk, D7 => id7, D711 => id711, D2312 => id2312, Eq => ieq, win => win, lose => lose, sp => isp, roll => iroll);
+	control : component controller port map(enter => enter, rst => rst, clk => iclk, D7 => id7, D711 => id711, D2312 => id2312, Eq => ieq, win => win, lose => lose, sp => isp, roll => iroll);
 	
 	-- instantiate the datapath --
-	data : component datapath port map(roll => iroll, sp => isp, clk => clk, d7 => id7, d711 => id711, d2312 => id2312, out1 => out1, out2 => out2, eq => ieq, rst => rst);
+	data : component datapath port map(roll => iroll, sp => isp, clk => iclk, d7 => id7, d711 => id711, d2312 => id2312, out1 => out1, out2 => out2, eq => ieq, rst => rst);
 	
 	outsp <= isp;
 
